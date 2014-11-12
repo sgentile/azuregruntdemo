986e0c70a5029a52455b0c72a917c4f87a68b2c1
Steve Gentile <steven.gentile@gmail.com>
2014-11-12 07:47:40 -0500

    fixed to use 'build'

--- deploy.sh
+++ deploy.sh
@@ -124,7 +124,7 @@ echo Starting Grunt
 if [ -e "$DEPLOYMENT_SOURCE/Gruntfile.js" ]; then
   eval $NPM_CMD install grunt-cli
   exitWithMessageOnError "installing grunt failed"
-  ./node_modules/.bin/grunt --no-color clean common dist
+  ./node_modules/.bin/grunt --no-color build 
   exitWithMessageOnError "grunt failed"
 fi
 
